f3197d2c1b37a1305fb5632378aa46c250e4eb7c
joseph-amirth <epicjoe128@gmail.com>
2024-12-25 15:50:59 +0530

    Extract common code for parsing/testing by EarleyParser into its own method

--- cfg-core/src/parsing/earley.rs
+++ cfg-core/src/parsing/earley.rs
@@ -19,102 +19,8 @@ impl<T: Term> EarleyParser<T> {
             rules_by_var,
         }
     }
-}
 
-impl<T: Term> Parser<Vec<T>> for EarleyParser<T> {
-    type TermType = T;
-
-    fn test(&self, word: Vec<T>) -> bool {
-        let n = word.len();
-
-        let mut states: Vec<Vec<State>> = vec![Vec::new(); n + 1];
-        for (i, _) in self.rules_by_var[self.start.0].iter().enumerate() {
-            states[0].push(State {
-                l: 0,
-                head: self.start,
-                body_idx: i,
-                parsed: 0,
-            });
-        }
-
-        for r in 0..=n {
-            let mut i = 0;
-            while i < states[r].len() {
-                let State {
-                    l,
-                    head,
-                    body_idx,
-                    parsed,
-                } = states[r][i];
-
-                let body = &self.rules_by_var[head.0][body_idx];
-
-                if parsed == body.len() {
-                    let mut j = 0;
-                    while j < states[l].len() {
-                        let state = states[l][j];
-                        let body = &self.rules_by_var[state.head.0][state.body_idx];
-                        if state.parsed == body.len() {
-                            j += 1;
-                            continue;
-                        }
-                        let Symbol::Var(var) = &body[state.parsed] else {
-                        j += 1;
-                            continue;
-                        };
-                        if *var == head {
-                            let state = State {
-                                parsed: state.parsed + 1,
-                                ..state
-                            };
-                            if !states[r].contains(&state) {
-                                states[r].push(state);
-                            }
-                        }
-                        j += 1;
-                    }
-                    i += 1;
-                    continue;
-                }
-
-                match &body[parsed] {
-                    Symbol::Var(var) => {
-                        for (i, _) in self.rules_by_var[var.0].iter().enumerate() {
-                            let state = State {
-                                l: r,
-                                head: *var,
-                                body_idx: i,
-                                parsed: 0,
-                            };
-                            if !states[r].contains(&state) {
-                                states[r].push(state);
-                            }
-                        }
-                    }
-                    Symbol::Term(term) => {
-                        if r < n && word[r] == *term {
-                            let state = State {
-                                l,
-                                head,
-                                body_idx,
-                                parsed: parsed + 1,
-                            };
-                            if !states[r + 1].contains(&state) {
-                                states[r + 1].push(state);
-                            }
-                        }
-                    }
-                }
-                i += 1;
-            }
-        }
-
-        states[n]
-            .iter()
-            .any(|state| state.l == 0 && state.head == self.start)
-    }
-
-    fn parse(&self, word: Vec<T>) -> Option<ParseTree<T>> {
+    fn attempt_parse(&self, word: &Vec<T>) -> (Vec<Vec<State>>, Vec<Vec<Parent>>) {
         let n = word.len();
 
         let mut states: Vec<Vec<State>> = vec![Vec::new(); n + 1];
@@ -205,11 +111,32 @@ impl<T: Term> Parser<Vec<T>> for EarleyParser<T> {
             }
         }
 
+        (states, parents)
+    }
+}
+
+impl<T: Term> Parser<Vec<T>> for EarleyParser<T> {
+    type TermType = T;
+
+    fn test(&self, word: Vec<T>) -> bool {
+        let n = word.len();
+        let (states, _) = self.attempt_parse(&word);
+
+        states[n]
+            .iter()
+            .any(|state| state.l == 0 && state.head == self.start)
+    }
+
+    fn parse(&self, word: Vec<T>) -> Option<ParseTree<T>> {
+        let n = word.len();
+        let (states, parents) = self.attempt_parse(&word);
+
         let Some(final_state_pos) = states[n]
             .iter()
-            .position(|state| state.l == 0 && state.head == self.start) else {
-                return None;
-            };
+            .position(|state| state.l == 0 && state.head == self.start)
+        else {
+            return None;
+        };
 
         Some(
             ParseTreeBuilder {
